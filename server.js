9995a497e48ea9da262f4af62d98704664260568
Gwack <gracalynntrammell@gmail.com>
2021-12-03 12:21:49 -0600

    created POST PUT and DELETE routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const db = require('./db/connection');
 const express = require('express');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -9,10 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// use apiRoutes
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
